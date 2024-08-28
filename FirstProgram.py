77a525021925e37fec559930e4479f74a65fb6b2
Eli3896 <142963430+Eli3896@users.noreply.github.com>
2024-08-28 16:49:22 +0000

    end of class

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,19 +1,22 @@
 #FirstProgram.py
-#Name:
-#Date:
+#Name:eli burklund
+#Date: 8/28
 #Assignment:
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
-
+  answer=input("what is your name?")
   #Use the user's name in the program.
-
+  print("thank you "+ answer)
   #Ask the user for their age.
-
+  age = input("what is your age?")
   #Tell the user what year they were born in.
+  age = int(age)
+  birthyear= 2024-age
+  print(birthyear)
   #Assume that they have not had their birthday yet this year.
 
 
--- MadLib.py
+++ MadLib.py
@@ -6,7 +6,13 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  adg1= input('give me a adgitive ')
+  noun1=input('give me a noun ')
+  verb1=input('give me a verb ')
+  place1=input('give me a place ')
+  noun2 = input('give me a noun ')
+  noun3= input('give me a noun ')
+  print("there was a "+adg1+' ' +noun1+" that was "+verb1+" then they went to the "+place1+" in a "+noun2+" with the "+noun3)
   #Print the story with the user supplied words.
 
 
